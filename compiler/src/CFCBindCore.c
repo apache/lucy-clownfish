kstarsinic/lucy

Improve support for multiple parcels
* Generate separate parcel.h and parcel.c files for every parcel.
* Support multiple parcels in a single binary.
* Use intra-parcel dependency information for includes and
  bootstrapping.

clownfish/compiler/src/CFCBindCore.c
@@ -22,7 +22,6 @@
 #define CFC_NEED_BASE_STRUCT_DEF
 #include "CFCBase.h"
 #include "CFCBindCore.h"
-#include "CFCBindAliases.h"
 #include "CFCBindClass.h"
 #include "CFCBindFile.h"
 #include "CFCClass.h"
@@ -45,12 +44,12 @@ struct CFCBindCore {
  * all classes, plus typedefs for all class structs.
  */
 static void
-S_write_parcel_h(CFCBindCore *self);
+S_write_parcel_h(CFCBindCore *self, CFCParcel *parcel);
 
 /* Write the "parcel.c" file containing autogenerated implementation code.
  */
 static void
-S_write_parcel_c(CFCBindCore *self);
+S_write_parcel_c(CFCBindCore *self, CFCParcel *parcel);
 
 static char*
 S_charmony_defines();
@@ -116,8 +115,13 @@ CFCBindCore_write_all_modified(CFCBindCore *self, int modified) {
     // If any class definition has changed, rewrite the parcel.h and parcel.c
     // files.
     if (modified) {
-        S_write_parcel_h(self);
-        S_write_parcel_c(self);
+        CFCParcel **parcels = CFCParcel_source_parcels();
+        for (size_t i = 0; parcels[i]; ++i) {
+            CFCParcel *parcel = parcels[i];
+            S_write_parcel_h(self, parcel);
+            S_write_parcel_c(self, parcel);
+        }
+        FREEMEM(parcels);
     }
 
     return modified;
@@ -127,69 +131,41 @@ CFCBindCore_write_all_modified(CFCBindCore *self, int modified) {
  * all classes, plus typedefs for all class structs.
  */
 static void
-S_write_parcel_h(CFCBindCore *self) {
-    CFCHierarchy *hierarchy = self->hierarchy;
-    CFCParcel    *parcel    = NULL;
+S_write_parcel_h(CFCBindCore *self, CFCParcel *parcel) {
+    CFCHierarchy *hierarchy   = self->hierarchy;
+    const char   *prefix      = CFCParcel_get_prefix(parcel);
+    const char   *PREFIX      = CFCParcel_get_PREFIX(parcel);
+    const char   *privacy_sym = CFCParcel_get_privacy_sym(parcel);
 
     char *charmony_defines = S_charmony_defines();
 
     // Declare object structs for all instantiable classes.
-    // Obtain parcel prefix for use in bootstrap function name.
     char *typedefs = CFCUtil_strdup("");
     CFCClass **ordered = CFCHierarchy_ordered_classes(hierarchy);
     for (int i = 0; ordered[i] != NULL; i++) {
         CFCClass *klass = ordered[i];
+        const char *class_prefix = CFCClass_get_prefix(klass);
+        if (strcmp(class_prefix, prefix) != 0) { continue; }
+
         if (!CFCClass_inert(klass)) {
             const char *full_struct = CFCClass_full_struct_sym(klass);
             typedefs = CFCUtil_cat(typedefs, "typedef struct ", full_struct,
                                    " ", full_struct, ";\n", NULL);
         }
-        if (!CFCClass_included(klass)) {
-            if (parcel && CFCClass_get_parcel(klass) != parcel) {
-                //CFCUtil_die("Multiple parcels not yet supported.");
-            }
-            parcel = CFCClass_get_parcel(klass);
-        }
     }
     FREEMEM(ordered);
 
-    if (!parcel) {
-        CFCUtil_die("No source classes found.");
-    }
-    //const char *prefix = CFCParcel_get_prefix(parcel);
-    //const char *PREFIX = CFCParcel_get_PREFIX(parcel);
-    const char *prefix = "lucy_";
-    const char *PREFIX = "LUCY_";
-
-    // Create Clownfish aliases if necessary.
-    char *aliases = CFCBindAliases_c_aliases();
-
-    const char pattern[] =
-        "%s\n"
-        "#ifndef CFCPARCEL_H\n"
-        "#define CFCPARCEL_H 1\n"
-        "\n"
-        "#ifdef __cplusplus\n"
-        "extern \"C\" {\n"
-        "#endif\n"
-        "\n"
+    // Special includes for Clownfish parcel.
+    const char *cfish_includes_pattern =
         "#include <stdarg.h>\n"
         "#include <stddef.h>\n"
         "\n"
         "%s"
         "\n"
-        "#include \"cfish_hostdefs.h\"\n"
-        "\n"
-        "#ifdef CFP_LUCY\n"
-        "  #include \"charmony.h\"\n"
-        "  #define LUCY_VISIBLE CFISH_EXPORT\n"
-        "#else\n"
-        "  #define LUCY_VISIBLE CFISH_IMPORT\n"
-        "#endif\n"
-        "\n"
-        "%s\n"
-        "%s\n"
-        "\n"
+        "#include \"cfish_hostdefs.h\"\n";
+
+    // Special definitions for Clownfish parcel.
+    const char *cfish_defs =
         "/* Generic method pointer.\n"
         " */\n"
         "typedef void\n"
@@ -269,6 +245,51 @@ S_write_parcel_h(CFCBindCore *self) {
         "  #define MethodSpec               cfish_MethodSpec\n"
         "  #define VTableSpec               cfish_VTableSpec\n"
         "#endif\n"
+        "\n";
+
+    const char *extra_defs;
+    char *extra_includes;
+    if (strcmp(prefix, "cfish_") == 0) {
+        extra_defs = cfish_defs;
+        extra_includes = CFCUtil_sprintf(cfish_includes_pattern,
+                                         charmony_defines);
+    }
+    else {
+        extra_defs = "";
+        extra_includes = CFCUtil_strdup("");
+
+        // Include parcel.h of dependent parcels.
+        CFCParcel **dep_parcels = CFCParcel_dependent_parcels(parcel);
+        for (size_t i = 0; dep_parcels[i]; ++i) {
+            const char *dep_prefix = CFCParcel_get_prefix(dep_parcels[i]);
+            extra_includes = CFCUtil_cat(extra_includes, "#include <",
+                                         dep_prefix, "parcel.h>\n", NULL);
+        }
+    }
+
+    const char pattern[] =
+        "%s\n"
+        "#ifndef CFISH_%sPARCEL_H\n"
+        "#define CFISH_%sPARCEL_H 1\n"
+        "\n"
+        "#ifdef __cplusplus\n"
+        "extern \"C\" {\n"
+        "#endif\n"
+        "\n"
+        "%s" // Extra includes.
+        "\n"
+        "#ifdef %s\n"
+        "  #include \"charmony.h\"\n"
+        "  #define %sVISIBLE CFISH_EXPORT\n"
+        "#else\n"
+        "  #define %sVISIBLE CFISH_IMPORT\n"
+        "#endif\n"
+        "\n"
+        "%s" // Typedefs.
+        "\n"
+        "%s" // Extra definitions.
+        "%sVISIBLE void\n"
+        "%sbootstrap_inheritance();\n"
         "\n"
         "%sVISIBLE void\n"
         "%sbootstrap_parcel();\n"
@@ -280,13 +301,15 @@ S_write_parcel_h(CFCBindCore *self) {
         "}\n"
         "#endif\n"
         "\n"
-        "#endif /* CFCPARCEL_H */\n"
+        "#endif /* CFISH_%sPARCEL_H */\n"
         "\n"
         "%s\n"
         "\n";
     char *file_content
-        = CFCUtil_sprintf(pattern, self->header, charmony_defines, aliases,
-                          typedefs, PREFIX, prefix, prefix, self->footer);
+        = CFCUtil_sprintf(pattern, self->header, PREFIX, PREFIX,
+                          extra_includes, privacy_sym, PREFIX, PREFIX,
+                          typedefs, extra_defs, PREFIX, prefix, PREFIX, prefix,
+                          prefix, PREFIX, self->footer);
 
     // Unlink then write file.
     const char *inc_dest = CFCHierarchy_get_include_dest(hierarchy);
@@ -297,18 +320,17 @@ S_write_parcel_h(CFCBindCore *self) {
     FREEMEM(filepath);
 
     FREEMEM(charmony_defines);
-    FREEMEM(aliases);
     FREEMEM(typedefs);
+    FREEMEM(extra_includes);
     FREEMEM(file_content);
 }
 
 static void
-S_write_parcel_c(CFCBindCore *self) {
+S_write_parcel_c(CFCBindCore *self, CFCParcel *parcel) {
     CFCHierarchy *hierarchy = self->hierarchy;
-    CFCParcel    *parcel    = NULL;
+    const char   *prefix    = CFCParcel_get_prefix(parcel);
 
-    // Aggregate C code from all files.
-    // Obtain parcel prefix for use in bootstrap function name.
+    // Aggregate C code for the parcel.
     char *privacy_syms = CFCUtil_strdup("");
     char *includes     = CFCUtil_strdup("");
     char *c_data       = CFCUtil_strdup("");
@@ -318,12 +340,12 @@ S_write_parcel_c(CFCBindCore *self) {
     CFCClass **ordered  = CFCHierarchy_ordered_classes(hierarchy);
     for (int i = 0; ordered[i] != NULL; i++) {
         CFCClass *klass = ordered[i];
+        const char *class_prefix = CFCClass_get_prefix(klass);
+        if (strcmp(class_prefix, prefix) != 0) { continue; }
 
         const char *include_h = CFCClass_include_h(klass);
         includes = CFCUtil_cat(includes, "#include \"", include_h,
                                "\"\n", NULL);
-
-        if (CFCClass_included(klass)) { continue; }
 
         CFCBindClass *class_binding = CFCBindClass_new(klass);
         char *class_c_data = CFCBindClass_to_c_data(class_binding);
@@ -342,28 +364,47 @@ S_write_parcel_c(CFCBindCore *self) {
         const char *privacy_sym = CFCClass_privacy_symbol(klass);
         privacy_syms = CFCUtil_cat(privacy_syms, "#define ",
                                    privacy_sym, "\n", NULL);
-        if (parcel && CFCClass_get_parcel(klass) != parcel) {
-            //CFCUtil_die("Multiple parcels not yet supported.");
-        }
-        parcel = CFCClass_get_parcel(klass);
     }
     vt_specs = CFCUtil_cat(vt_specs, "\n};\n", NULL);
     FREEMEM(ordered);
 
-    if (!parcel) {
-        CFCUtil_die("No source classes found.");
+    // Bootstrapping code for dependent parcels.
+    //
+    // bootstrap_inheritance() first calls bootstrap_inheritance() for all
+    // parcels from which classes are extended. Then the VTables of the parcel
+    // are initialized. It aborts on recursive invocation.
+    //
+    // bootstrap_parcel() first calls bootstrap_inheritance() of its own
+    // parcel. Then it calls bootstrap_parcel() for all dependent parcels.
+    // Finally, it calls init_parcel(). Recursive invocation is allowed.
+
+    char *ext_bootstrap = CFCUtil_strdup("");
+    char *dep_bootstrap = CFCUtil_strdup("");
+    CFCParcel **ext_parcels = CFCParcel_extended_parcels(parcel);
+    for (size_t i = 0; ext_parcels[i]; ++i) {
+        const char *ext_prefix = CFCParcel_get_prefix(ext_parcels[i]);
+        ext_bootstrap = CFCUtil_cat(ext_bootstrap, "    ", ext_prefix,
+                                    "bootstrap_inheritance();\n", NULL);
     }
-    //const char *prefix = CFCParcel_get_prefix(parcel);
-    const char *prefix = "lucy_";
+    CFCParcel **dep_parcels = CFCParcel_dependent_parcels(parcel);
+    for (size_t i = 0; dep_parcels[i]; ++i) {
+        const char *dep_prefix = CFCParcel_get_prefix(dep_parcels[i]);
+        dep_bootstrap = CFCUtil_cat(dep_bootstrap, "    ", dep_prefix,
+                                    "bootstrap_parcel();\n", NULL);
+    }
 
     char pattern[] =
         "%s\n"
         "\n"
-        "#define C_CFISH_VTABLE\n"
+        "#define C_CFISH_VTABLE\n"          // Needed for method_ptrs offset.
+        "#include <stdio.h>\n"
+        "#include <stdlib.h>\n"
         "%s\n"
         "#include \"%sparcel.h\"\n"
         "#include \"callbacks.h\"\n"
-        "#include \"Clownfish/VTable.h\"\n"
+        "#include \"Clownfish/VTable.h\"\n" // Needed for method_ptrs offset.
+        "#include \"Clownfish/VArray.h\"\n" // Needed for dump/load.
+        "#include \"Clownfish/Num.h\"\n"    // Needed for dump/load.
         "%s\n"
         "\n"
         "%s\n"
@@ -372,20 +413,37 @@ S_write_parcel_c(CFCBindCore *self) {
         " */\n"
         "%s\n"
         "\n"
+        "static int bootstrap_state = 0;\n"
+        "\n"
+        "void\n"
+        "%sbootstrap_inheritance() {\n"
+        "    if (bootstrap_state == 1) {\n"
+        "        fprintf(stderr, \"Cycle in class inheritance between\"\n"
+        "                        \" parcels detected.\\n\");\n"
+        "        abort();\n"
+        "    }\n"
+        "    if (bootstrap_state >= 2) { return; }\n"
+        "    bootstrap_state = 1;\n"
+        "%s" // Bootstrap extended parcels.
+        "    cfish_VTable_bootstrap(vtable_specs, %d);\n"
+        "    bootstrap_state = 2;\n"
+        "}\n"
+        "\n"
         "void\n"
         "%sbootstrap_parcel() {\n"
-        "    static bool bootstrapped = false;\n"
-        "    if (bootstrapped) { return; }\n"
-        "    cfish_VTable_bootstrap(vtable_specs, %d);\n"
+        "    if (bootstrap_state >= 3) { return; }\n"
+        "    %sbootstrap_inheritance();\n"
+        "    bootstrap_state = 3;\n"
+        "%s" // Finish bootstrapping of all dependent parcels.
         "    %sinit_parcel();\n"
-        "    bootstrapped = true;\n"
         "}\n"
         "\n"
         "%s\n";
     char *file_content
         = CFCUtil_sprintf(pattern, self->header, privacy_syms, prefix,
-                          includes, c_data, vt_specs, prefix, num_specs,
-                          prefix, self->footer);
+                          includes, c_data, vt_specs, prefix, ext_bootstrap,
+                          num_specs, prefix, prefix, dep_bootstrap, prefix,
+                          self->footer);
 
     // Unlink then open file.
     const char *src_dest = CFCHierarchy_get_source_dest(hierarchy);
@@ -399,6 +457,8 @@ S_write_parcel_c(CFCBindCore *self) {
     FREEMEM(includes);
     FREEMEM(c_data);
     FREEMEM(vt_specs);
+    FREEMEM(ext_bootstrap);
+    FREEMEM(dep_bootstrap);
     FREEMEM(file_content);
 }
 
@@ -409,7 +469,6 @@ void
 CFCBindCore_write_callbacks_h(CFCBindCore *self) {
     CFCHierarchy  *hierarchy   = self->hierarchy;
     CFCClass     **ordered     = CFCHierarchy_ordered_classes(hierarchy);
-    CFCParcel     *parcel      = NULL;
     char          *includes    = CFCUtil_strdup("");
     char          *all_cb_decs = CFCUtil_strdup("");
 
@@ -426,21 +485,10 @@ CFCBindCore_write_callbacks_h(CFCBindCore *self) {
             all_cb_decs = CFCUtil_cat(all_cb_decs, cb_decs, NULL);
             FREEMEM(cb_decs);
             CFCBase_decref((CFCBase*)class_binding);
-
-            if (parcel && CFCClass_get_parcel(klass) != parcel) {
-                //CFCUtil_die("Multiple parcels not yet supported.");
-            }
-            parcel = CFCClass_get_parcel(klass);
         }
     }
 
     FREEMEM(ordered);
-
-    if (!parcel) {
-        CFCUtil_die("No source classes found.");
-    }
-    //const char *prefix = CFCParcel_get_prefix(parcel);
-    const char *prefix = "lucy_";
 
     const char pattern[] =
         "%s\n"
@@ -451,7 +499,6 @@ CFCBindCore_write_callbacks_h(CFCBindCore *self) {
         "extern \"C\" {\n"
         "#endif\n"
         "\n"
-        "#include \"%sparcel.h\"\n"
         "%s"
         "\n"
         "%s"
@@ -465,7 +512,7 @@ CFCBindCore_write_callbacks_h(CFCBindCore *self) {
         "%s\n"
         "\n";
     char *file_content
-        = CFCUtil_sprintf(pattern, self->header, prefix, includes, all_cb_decs,
+        = CFCUtil_sprintf(pattern, self->header, includes, all_cb_decs,
                           self->footer);
 
     // Unlink then write file.

apache/lucy-clownfish

compiler/src/CFCBindCore.c
@@ -22,7 +22,6 @@
 #define CFC_NEED_BASE_STRUCT_DEF
 #include "CFCBase.h"
 #include "CFCBindCore.h"
-#include "CFCBindAliases.h"
 #include "CFCBindClass.h"
 #include "CFCBindFile.h"
 #include "CFCClass.h"
@@ -45,12 +44,12 @@ struct CFCBindCore {
  * all classes, plus typedefs for all class structs.
  */
 static void
-S_write_parcel_h(CFCBindCore *self);
+S_write_parcel_h(CFCBindCore *self, CFCParcel *parcel);
 
 /* Write the "parcel.c" file containing autogenerated implementation code.
  */
 static void
-S_write_parcel_c(CFCBindCore *self);
+S_write_parcel_c(CFCBindCore *self, CFCParcel *parcel);
 
 static char*
 S_charmony_defines();
@@ -116,8 +115,13 @@ CFCBindCore_write_all_modified(CFCBindCore *self, int modified) {
     // If any class definition has changed, rewrite the parcel.h and parcel.c
     // files.
     if (modified) {
-        S_write_parcel_h(self);
-        S_write_parcel_c(self);
+        CFCParcel **parcels = CFCParcel_source_parcels();
+        for (size_t i = 0; parcels[i]; ++i) {
+            CFCParcel *parcel = parcels[i];
+            S_write_parcel_h(self, parcel);
+            S_write_parcel_c(self, parcel);
+        }
+        FREEMEM(parcels);
     }
 
     return modified;
@@ -127,69 +131,41 @@ CFCBindCore_write_all_modified(CFCBindCore *self, int modified) {
  * all classes, plus typedefs for all class structs.
  */
 static void
-S_write_parcel_h(CFCBindCore *self) {
-    CFCHierarchy *hierarchy = self->hierarchy;
-    CFCParcel    *parcel    = NULL;
+S_write_parcel_h(CFCBindCore *self, CFCParcel *parcel) {
+    CFCHierarchy *hierarchy   = self->hierarchy;
+    const char   *prefix      = CFCParcel_get_prefix(parcel);
+    const char   *PREFIX      = CFCParcel_get_PREFIX(parcel);
+    const char   *privacy_sym = CFCParcel_get_privacy_sym(parcel);
 
     char *charmony_defines = S_charmony_defines();
 
     // Declare object structs for all instantiable classes.
-    // Obtain parcel prefix for use in bootstrap function name.
     char *typedefs = CFCUtil_strdup("");
     CFCClass **ordered = CFCHierarchy_ordered_classes(hierarchy);
     for (int i = 0; ordered[i] != NULL; i++) {
         CFCClass *klass = ordered[i];
+        const char *class_prefix = CFCClass_get_prefix(klass);
+        if (strcmp(class_prefix, prefix) != 0) { continue; }
+
         if (!CFCClass_inert(klass)) {
             const char *full_struct = CFCClass_full_struct_sym(klass);
             typedefs = CFCUtil_cat(typedefs, "typedef struct ", full_struct,
                                    " ", full_struct, ";\n", NULL);
         }
-        if (!CFCClass_included(klass)) {
-            if (parcel && CFCClass_get_parcel(klass) != parcel) {
-                //CFCUtil_die("Multiple parcels not yet supported.");
-            }
-            parcel = CFCClass_get_parcel(klass);
-        }
     }
     FREEMEM(ordered);
 
-    if (!parcel) {
-        CFCUtil_die("No source classes found.");
-    }
-    //const char *prefix = CFCParcel_get_prefix(parcel);
-    //const char *PREFIX = CFCParcel_get_PREFIX(parcel);
-    const char *prefix = "lucy_";
-    const char *PREFIX = "LUCY_";
-
-    // Create Clownfish aliases if necessary.
-    char *aliases = CFCBindAliases_c_aliases();
-
-    const char pattern[] =
-        "%s\n"
-        "#ifndef CFCPARCEL_H\n"
-        "#define CFCPARCEL_H 1\n"
-        "\n"
-        "#ifdef __cplusplus\n"
-        "extern \"C\" {\n"
-        "#endif\n"
-        "\n"
+    // Special includes for Clownfish parcel.
+    const char *cfish_includes_pattern =
         "#include <stdarg.h>\n"
         "#include <stddef.h>\n"
         "\n"
         "%s"
         "\n"
-        "#include \"cfish_hostdefs.h\"\n"
-        "\n"
-        "#ifdef CFP_LUCY\n"
-        "  #include \"charmony.h\"\n"
-        "  #define LUCY_VISIBLE CFISH_EXPORT\n"
-        "#else\n"
-        "  #define LUCY_VISIBLE CFISH_IMPORT\n"
-        "#endif\n"
-        "\n"
-        "%s\n"
-        "%s\n"
-        "\n"
+        "#include \"cfish_hostdefs.h\"\n";
+
+    // Special definitions for Clownfish parcel.
+    const char *cfish_defs =
         "/* Generic method pointer.\n"
         " */\n"
         "typedef void\n"
@@ -269,6 +245,52 @@ S_write_parcel_h(CFCBindCore *self) {
         "  #define MethodSpec               cfish_MethodSpec\n"
         "  #define VTableSpec               cfish_VTableSpec\n"
         "#endif\n"
+        "\n";
+
+    const char *extra_defs;
+    char *extra_includes;
+    if (strcmp(prefix, "cfish_") == 0) {
+        extra_defs = cfish_defs;
+        extra_includes = CFCUtil_sprintf(cfish_includes_pattern,
+                                         charmony_defines);
+    }
+    else {
+        extra_defs = "";
+        extra_includes = CFCUtil_strdup("");
+
+        // Include parcel.h of dependent parcels.
+        CFCParcel **dep_parcels = CFCParcel_dependent_parcels(parcel);
+        for (size_t i = 0; dep_parcels[i]; ++i) {
+            const char *dep_prefix = CFCParcel_get_prefix(dep_parcels[i]);
+            extra_includes = CFCUtil_cat(extra_includes, "#include <",
+                                         dep_prefix, "parcel.h>\n", NULL);
+        }
+        FREEMEM(dep_parcels);
+    }
+
+    const char pattern[] =
+        "%s\n"
+        "#ifndef CFISH_%sPARCEL_H\n"
+        "#define CFISH_%sPARCEL_H 1\n"
+        "\n"
+        "#ifdef __cplusplus\n"
+        "extern \"C\" {\n"
+        "#endif\n"
+        "\n"
+        "%s" // Extra includes.
+        "\n"
+        "#ifdef %s\n"
+        "  #include \"charmony.h\"\n"
+        "  #define %sVISIBLE CFISH_EXPORT\n"
+        "#else\n"
+        "  #define %sVISIBLE CFISH_IMPORT\n"
+        "#endif\n"
+        "\n"
+        "%s" // Typedefs.
+        "\n"
+        "%s" // Extra definitions.
+        "%sVISIBLE void\n"
+        "%sbootstrap_inheritance();\n"
         "\n"
         "%sVISIBLE void\n"
         "%sbootstrap_parcel();\n"
@@ -280,13 +302,15 @@ S_write_parcel_h(CFCBindCore *self) {
         "}\n"
         "#endif\n"
         "\n"
-        "#endif /* CFCPARCEL_H */\n"
+        "#endif /* CFISH_%sPARCEL_H */\n"
         "\n"
         "%s\n"
         "\n";
     char *file_content
-        = CFCUtil_sprintf(pattern, self->header, charmony_defines, aliases,
-                          typedefs, PREFIX, prefix, prefix, self->footer);
+        = CFCUtil_sprintf(pattern, self->header, PREFIX, PREFIX,
+                          extra_includes, privacy_sym, PREFIX, PREFIX,
+                          typedefs, extra_defs, PREFIX, prefix, PREFIX, prefix,
+                          prefix, PREFIX, self->footer);
 
     // Unlink then write file.
     const char *inc_dest = CFCHierarchy_get_include_dest(hierarchy);
@@ -297,18 +321,17 @@ S_write_parcel_h(CFCBindCore *self) {
     FREEMEM(filepath);
 
     FREEMEM(charmony_defines);
-    FREEMEM(aliases);
     FREEMEM(typedefs);
+    FREEMEM(extra_includes);
     FREEMEM(file_content);
 }
 
 static void
-S_write_parcel_c(CFCBindCore *self) {
+S_write_parcel_c(CFCBindCore *self, CFCParcel *parcel) {
     CFCHierarchy *hierarchy = self->hierarchy;
-    CFCParcel    *parcel    = NULL;
+    const char   *prefix    = CFCParcel_get_prefix(parcel);
 
-    // Aggregate C code from all files.
-    // Obtain parcel prefix for use in bootstrap function name.
+    // Aggregate C code for the parcel.
     char *privacy_syms = CFCUtil_strdup("");
     char *includes     = CFCUtil_strdup("");
     char *c_data       = CFCUtil_strdup("");
@@ -318,12 +341,12 @@ S_write_parcel_c(CFCBindCore *self) {
     CFCClass **ordered  = CFCHierarchy_ordered_classes(hierarchy);
     for (int i = 0; ordered[i] != NULL; i++) {
         CFCClass *klass = ordered[i];
+        const char *class_prefix = CFCClass_get_prefix(klass);
+        if (strcmp(class_prefix, prefix) != 0) { continue; }
 
         const char *include_h = CFCClass_include_h(klass);
         includes = CFCUtil_cat(includes, "#include \"", include_h,
                                "\"\n", NULL);
-
-        if (CFCClass_included(klass)) { continue; }
 
         CFCBindClass *class_binding = CFCBindClass_new(klass);
         char *class_c_data = CFCBindClass_to_c_data(class_binding);
@@ -342,28 +365,51 @@ S_write_parcel_c(CFCBindCore *self) {
         const char *privacy_sym = CFCClass_privacy_symbol(klass);
         privacy_syms = CFCUtil_cat(privacy_syms, "#define ",
                                    privacy_sym, "\n", NULL);
-        if (parcel && CFCClass_get_parcel(klass) != parcel) {
-            //CFCUtil_die("Multiple parcels not yet supported.");
-        }
-        parcel = CFCClass_get_parcel(klass);
     }
     vt_specs = CFCUtil_cat(vt_specs, "\n};\n", NULL);
     FREEMEM(ordered);
 
-    if (!parcel) {
-        CFCUtil_die("No source classes found.");
+    // Bootstrapping code for dependent parcels.
+    //
+    // bootstrap_inheritance() first calls bootstrap_inheritance() for all
+    // parcels from which classes are inherited. Then the VTables of the parcel
+    // are initialized. It aborts on recursive invocation.
+    //
+    // bootstrap_parcel() first calls bootstrap_inheritance() of its own
+    // parcel. Then it calls bootstrap_parcel() for all dependent parcels.
+    // Finally, it calls init_parcel(). Recursive invocation is allowed.
+
+    char *inh_bootstrap = CFCUtil_strdup("");
+    char *dep_bootstrap = CFCUtil_strdup("");
+    CFCParcel **inh_parcels = CFCParcel_inherited_parcels(parcel);
+    for (size_t i = 0; inh_parcels[i]; ++i) {
+        const char *inh_prefix = CFCParcel_get_prefix(inh_parcels[i]);
+        inh_bootstrap = CFCUtil_cat(inh_bootstrap, "    ", inh_prefix,
+                                    "bootstrap_inheritance();\n", NULL);
     }
-    //const char *prefix = CFCParcel_get_prefix(parcel);
-    const char *prefix = "lucy_";
+    FREEMEM(inh_parcels);
+    CFCParcel **dep_parcels = CFCParcel_dependent_parcels(parcel);
+    for (size_t i = 0; dep_parcels[i]; ++i) {
+        const char *dep_prefix = CFCParcel_get_prefix(dep_parcels[i]);
+        dep_bootstrap = CFCUtil_cat(dep_bootstrap, "    ", dep_prefix,
+                                    "bootstrap_parcel();\n", NULL);
+    }
+    FREEMEM(dep_parcels);
 
     char pattern[] =
         "%s\n"
         "\n"
-        "#define C_CFISH_VTABLE\n"
+        "#define C_CFISH_VTABLE\n"          // Needed for method_ptrs offset.
+        "#include <stdio.h>\n"
+        "#include <stdlib.h>\n"
         "%s\n"
         "#include \"%sparcel.h\"\n"
         "#include \"callbacks.h\"\n"
-        "#include \"Clownfish/VTable.h\"\n"
+        "#include \"Clownfish/CharBuf.h\"\n" // Needed for dump/load.
+        "#include \"Clownfish/Err.h\"\n"     // Needed for dump/load.
+        "#include \"Clownfish/Num.h\"\n"     // Needed for dump/load.
+        "#include \"Clownfish/VArray.h\"\n"  // Needed for dump/load.
+        "#include \"Clownfish/VTable.h\"\n"  // Needed for method_ptrs offset.
         "%s\n"
         "\n"
         "%s\n"
@@ -372,20 +418,37 @@ S_write_parcel_c(CFCBindCore *self) {
         " */\n"
         "%s\n"
         "\n"
+        "static int bootstrap_state = 0;\n"
+        "\n"
+        "void\n"
+        "%sbootstrap_inheritance() {\n"
+        "    if (bootstrap_state == 1) {\n"
+        "        fprintf(stderr, \"Cycle in class inheritance between\"\n"
+        "                        \" parcels detected.\\n\");\n"
+        "        abort();\n"
+        "    }\n"
+        "    if (bootstrap_state >= 2) { return; }\n"
+        "    bootstrap_state = 1;\n"
+        "%s" // Bootstrap inherited parcels.
+        "    cfish_VTable_bootstrap(vtable_specs, %d);\n"
+        "    bootstrap_state = 2;\n"
+        "}\n"
+        "\n"
         "void\n"
         "%sbootstrap_parcel() {\n"
-        "    static bool bootstrapped = false;\n"
-        "    if (bootstrapped) { return; }\n"
-        "    cfish_VTable_bootstrap(vtable_specs, %d);\n"
+        "    if (bootstrap_state >= 3) { return; }\n"
+        "    %sbootstrap_inheritance();\n"
+        "    bootstrap_state = 3;\n"
+        "%s" // Finish bootstrapping of all dependent parcels.
         "    %sinit_parcel();\n"
-        "    bootstrapped = true;\n"
         "}\n"
         "\n"
         "%s\n";
     char *file_content
         = CFCUtil_sprintf(pattern, self->header, privacy_syms, prefix,
-                          includes, c_data, vt_specs, prefix, num_specs,
-                          prefix, self->footer);
+                          includes, c_data, vt_specs, prefix, inh_bootstrap,
+                          num_specs, prefix, prefix, dep_bootstrap, prefix,
+                          self->footer);
 
     // Unlink then open file.
     const char *src_dest = CFCHierarchy_get_source_dest(hierarchy);
@@ -399,6 +462,8 @@ S_write_parcel_c(CFCBindCore *self) {
     FREEMEM(includes);
     FREEMEM(c_data);
     FREEMEM(vt_specs);
+    FREEMEM(inh_bootstrap);
+    FREEMEM(dep_bootstrap);
     FREEMEM(file_content);
 }
 
@@ -409,7 +474,6 @@ void
 CFCBindCore_write_callbacks_h(CFCBindCore *self) {
     CFCHierarchy  *hierarchy   = self->hierarchy;
     CFCClass     **ordered     = CFCHierarchy_ordered_classes(hierarchy);
-    CFCParcel     *parcel      = NULL;
     char          *includes    = CFCUtil_strdup("");
     char          *all_cb_decs = CFCUtil_strdup("");
 
@@ -426,21 +490,10 @@ CFCBindCore_write_callbacks_h(CFCBindCore *self) {
             all_cb_decs = CFCUtil_cat(all_cb_decs, cb_decs, NULL);
             FREEMEM(cb_decs);
             CFCBase_decref((CFCBase*)class_binding);
-
-            if (parcel && CFCClass_get_parcel(klass) != parcel) {
-                //CFCUtil_die("Multiple parcels not yet supported.");
-            }
-            parcel = CFCClass_get_parcel(klass);
         }
     }
 
     FREEMEM(ordered);
-
-    if (!parcel) {
-        CFCUtil_die("No source classes found.");
-    }
-    //const char *prefix = CFCParcel_get_prefix(parcel);
-    const char *prefix = "lucy_";
 
     const char pattern[] =
         "%s\n"
@@ -451,7 +504,6 @@ CFCBindCore_write_callbacks_h(CFCBindCore *self) {
         "extern \"C\" {\n"
         "#endif\n"
         "\n"
-        "#include \"%sparcel.h\"\n"
         "%s"
         "\n"
         "%s"
@@ -465,7 +517,7 @@ CFCBindCore_write_callbacks_h(CFCBindCore *self) {
         "%s\n"
         "\n";
     char *file_content
-        = CFCUtil_sprintf(pattern, self->header, prefix, includes, all_cb_decs,
+        = CFCUtil_sprintf(pattern, self->header, includes, all_cb_decs,
                           self->footer);
 
     // Unlink then write file.