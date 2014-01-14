b14e4051d637f01aa2b19a126838dc47c7516b69
Nick Wellnhofer <wellnhofer@aevum.de>
2014-01-14 21:42:12 +0100

    Regenerate charmonizer.c

--- compiler/common/charmonizer.c
+++ compiler/common/charmonizer.c
@@ -6991,7 +6991,7 @@ chaz_VariadicMacros_run(void) {
 
 
 
-#line 1 "clownfish/compiler/common/charmonizer.main"
+#line 1 "compiler/common/charmonizer.main"
 /* Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
--- runtime/common/charmonizer.c
+++ runtime/common/charmonizer.c
@@ -6991,7 +6991,7 @@ chaz_VariadicMacros_run(void) {
 
 
 
-#line 1 "clownfish/runtime/common/charmonizer.main"
+#line 1 "runtime/common/charmonizer.main"
 /* Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
