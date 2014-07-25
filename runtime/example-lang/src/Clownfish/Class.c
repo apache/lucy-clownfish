4e8f13076b1923001abd4fc4182a6600d3804cab
Nick Wellnhofer <wellnhofer@aevum.de>
2014-07-25 14:19:33 +0200

    Copy Clownfish part of example-lang from Lucy
    Also adjust for some interface changes.

---
+++ runtime/example-lang/src/Clownfish/Class.c
@@ -0,0 +1,62 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define C_CFISH_OBJ
+#define C_CFISH_CLASS
+
+#include "CFBind.h"
+
+cfish_Obj*
+CFISH_Class_Make_Obj_IMP(cfish_Class *self) {
+    THROW(CFISH_ERR, "TODO");
+    UNREACHABLE_RETURN(cfish_Obj*);
+}
+
+cfish_Obj*
+CFISH_Class_Init_Obj_IMP(cfish_Class *self, void *allocation) {
+    THROW(CFISH_ERR, "TODO");
+    UNREACHABLE_RETURN(cfish_Obj*);
+}
+
+cfish_Obj*
+CFISH_Class_Foster_Obj_IMP(cfish_Class *self, void *host_obj) {
+    THROW(CFISH_ERR, "TODO");
+    UNREACHABLE_RETURN(cfish_Obj*);
+}
+
+void
+cfish_Class_register_with_host(cfish_Class *singleton, cfish_Class *parent) {
+    THROW(CFISH_ERR, "TODO");
+}
+
+cfish_VArray*
+cfish_Class_fresh_host_methods(const cfish_String *class_name) {
+    THROW(CFISH_ERR, "TODO");
+    UNREACHABLE_RETURN(cfish_VArray*);
+}
+
+cfish_String*
+cfish_Class_find_parent_class(const cfish_String *class_name) {
+    THROW(CFISH_ERR, "TODO");
+    UNREACHABLE_RETURN(cfish_String*);
+}
+
+void*
+CFISH_Class_To_Host_IMP(cfish_Class *self) {
+    THROW(CFISH_ERR, "TODO");
+    UNREACHABLE_RETURN(void*);
+}
+
---
+++ runtime/example-lang/src/Clownfish/Err.c
@@ -0,0 +1,60 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "CFBind.h"
+
+void
+cfish_Err_init_class() {
+}
+
+cfish_Err*
+cfish_Err_get_error() {
+    THROW(CFISH_ERR, "TODO");
+    UNREACHABLE_RETURN(cfish_Err*);
+}
+
+void
+cfish_Err_set_error(cfish_Err *error) {
+    THROW(CFISH_ERR, "TODO");
+}
+
+void
+cfish_Err_do_throw(cfish_Err *err) {
+    THROW(CFISH_ERR, "TODO");
+}
+
+void*
+CFISH_Err_To_Host_IMP(cfish_Err *self) {
+    THROW(CFISH_ERR, "TODO");
+    UNREACHABLE_RETURN(void*);
+}
+
+void
+cfish_Err_throw_mess(cfish_Class *klass, cfish_String *message) {
+    THROW(CFISH_ERR, "TODO");
+}
+
+void
+cfish_Err_warn_mess(cfish_String *message) {
+    THROW(CFISH_ERR, "TODO");
+}
+
+cfish_Err*
+cfish_Err_trap(CFISH_Err_Attempt_t routine, void *context) {
+    THROW(CFISH_ERR, "TODO");
+    UNREACHABLE_RETURN(cfish_Err*);
+}
+
