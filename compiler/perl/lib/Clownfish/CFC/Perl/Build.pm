ac2de384eda638a8e3dd49d32764a634aa0ff44e
Nick Wellnhofer <wellnhofer@aevum.de>
2015-03-30 14:18:03 +0200

    Improve check for Windows dlext
    Strawberry Perl 5.20 uses `xs.dll` which broke the Lucy build.

--- compiler/perl/lib/Clownfish/CFC/Perl/Build.pm
+++ compiler/perl/lib/Clownfish/CFC/Perl/Build.pm
@@ -148,7 +148,7 @@ sub cf_linker_flags {
 
     my $dlext = $Config{dlext};
     # Only needed on Windows
-    return () if $dlext ne 'dll';
+    return () if $dlext !~ /dll$/i;
 
     # Link against import library on MSVC
     my $ext = $Config{cc} =~ /^cl\b/ ? 'lib' : $dlext;
