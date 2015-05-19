b26a9c605fdf432864f82901305225e8006343d1
Marvin Humphrey <marvin@rectangular.com>
2015-05-19 11:06:10 -0700

    Add a custom script for testing with Travic CI.
    *   Run tests for both CFC and the Clownfish runtime.
    *   Run tests for C, Perl and Go.

---
+++ devel/bin/travis-test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Exit if any command returns non-zero.
+set -e
+
+# Print all commands before executing.
+set -v
+
+test_c() {
+    cd compiler/c
+    ./configure
+    make -j test
+    cd ../../runtime/c
+    ./configure
+    make -j test
+}
+
+test_perl() {
+    perlbrew switch $PERL_VERSION
+    cd compiler/perl
+    cpanm --quiet --installdeps --notest .
+    perl Build.PL
+    ./Build test
+    cd ../../runtime/perl
+    cpanm --quiet --installdeps --notest .
+    perl Build.PL
+    ./Build test
+}
+
+test_go() {
+    mkdir -p gotest/src/git-wip-us.apache.org/repos/asf
+    ln -s `pwd` \
+        gotest/src/git-wip-us.apache.org/repos/asf/lucy-clownfish.git
+    export GOPATH="$(pwd)/gotest"
+    cd compiler/go
+    go run build.go test
+    go run build.go install
+    cd ../../runtime/go
+    go run build.go test
+}
+
+case $CLOWNFISH_HOST in
+    perl)
+        test_perl
+        ;;
+    c)
+        test_c
+        ;;
+    go)
+        test_go
+        ;;
+    *)
+        echo "unknown CLOWNFISH_HOST: $CLOWNFISH_HOST"
+        exit 1
+esac
+
