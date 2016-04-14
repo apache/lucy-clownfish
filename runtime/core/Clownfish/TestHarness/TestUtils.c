116a44fe19411862285a0b0d20be3970b3ed7113
Nick Wellnhofer <wellnhofer@aevum.de>
2016-04-14 14:47:05 +0200

    Fix TestUtils_time with 32-bit time_t
    tv_sec must be converted to uint64_t before the multiplication.

--- runtime/core/Clownfish/TestHarness/TestUtils.c
+++ runtime/core/Clownfish/TestHarness/TestUtils.c
@@ -148,7 +148,7 @@ TestUtils_time() {
     struct timeval t;
     gettimeofday(&t, NULL);
 
-    return (uint64_t)(t.tv_sec * 1000000 + t.tv_usec);
+    return (uint64_t)t.tv_sec * 1000000 + (uint64_t)t.tv_usec;
 }
 
 #else
